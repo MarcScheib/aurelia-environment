10a3cd6c8c9fb844ea79493df33b9f4fddea6a98
Marc Scheib <marc.scheib@gmx.de>
2016-04-20 20:28:33 +0200

    refactor(env): migrate from XHR to fetch

--- test/unit/index.spec.js
+++ test/unit/index.spec.js
@@ -1,15 +1,55 @@
 import {load} from '../../src/index';
 
 describe('the environment load routine', () => {
+  let originalFetch = window.fetch;
+  let fetch;
+
   beforeEach(() => {
-    spyOn(XMLHttpRequest.prototype, 'open').and.callThrough();
-    spyOn(XMLHttpRequest.prototype, 'send');
+    fetch = window.fetch = jasmine.createSpy('fetch');
   });
 
-  it('should request environment file', done => {
-    load();
-    expect(XMLHttpRequest.prototype.open).toHaveBeenCalled();
-    expect(XMLHttpRequest.prototype.send).toHaveBeenCalled();
-    done();
+  afterEach(() => {
+    fetch = window.fetch = originalFetch;
+  });
+
+  it('should error on missing fetch implementation', () => {
+    window.fetch = undefined;
+    expect(() => load()).toThrow();
+  });
+
+  it('should reject on missing environment file', done => {
+    let response = new Response(null, { status: 500 });
+    fetch.and.returnValue(Promise.resolve(response));
+
+    load({file: 'notExisting.env'})
+      .then(() => {
+        expect(true).not.toBe(true);
+      })
+      .catch(error => {
+        expect(error.status).toBeDefined();
+        expect(error.statusText).toBeDefined();
+      })
+      .then(() => {
+        expect(fetch).toHaveBeenCalled();
+        done();
+      });
+  });
+
+  it('should specify the environment', done => {
+    let response = new Response('key=value', { status: 200 });
+    fetch.and.returnValue(Promise.resolve(response));
+
+    load({file: '.env'})
+      .then(() => {
+        expect(window.env).toBeDefined();
+        expect(window.env.key).toEqual('value');
+      })
+      .catch(error => {
+        expect(error).not.toBe(error);
+      })
+      .then(() => {
+        expect(fetch).toHaveBeenCalled();
+        done();
+      });
   });
 });
